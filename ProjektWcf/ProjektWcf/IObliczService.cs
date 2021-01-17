f66967e9d3d5f4e9f9eed0afe0a4d1b6a3343a3c
k.r <karolina.rolnik@coig.pl>
2021-01-17 18:27:11 +0100

    add first folder

---
+++ ProjektWcf/ProjektWcf/IObliczService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProjektWcf
+{
+    // UWAGA: możesz użyć polecenia „Zmień nazwę” w menu „Refaktoryzuj”, aby zmienić nazwę interfejsu „IObliczService” w kodzie i pliku konfiguracji.
+    [ServiceContract]
+    public interface IObliczService
+    {
+        [OperationContract]
+        double srednieSpalanie(double paliwo, double km);
+
+        [OperationContract]
+        double koszt1km(double paliwo, double km, double cena);
+
+        [OperationContract]
+        double koszt100km(double paliwo, double km, double cena);
+
+        [OperationContract]
+        double kosztXkm(double paliwo, double km, double cena);
+    }
+}
---
+++ ProjektWcf/ProjektWcf/ObliczService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProjektWcf
+{
+    // UWAGA: możesz użyć polecenia „Zmień nazwę” w menu „Refaktoryzuj”, aby zmienić nazwę klasy „ObliczService” w kodzie i pliku konfiguracji.
+    public class ObliczService : IObliczService
+    {
+
+        public double srednieSpalanie(double paliwo, double km )
+        {
+            double wynik;
+
+            wynik = (paliwo / km) * 100;
+            return wynik;
+        }
+
+        public double koszt1km(double paliwo, double km, double cena)
+        {
+            double wynik;
+
+            wynik = (paliwo / km) * cena;
+            return wynik;
+        }
+
+        public double koszt100km(double paliwo, double km, double cena)
+        {
+            double wynik;
+
+            wynik = koszt1km(paliwo, km, cena) * 100;
+            return wynik;
+        }
+
+        public double kosztXkm(double paliwo, double km, double cena)
+        {
+            double wynik;
+
+            wynik = koszt1km(paliwo, km, cena) * km;
+            return wynik;
+        }
+    }
+}
